7ff308b777c9baa690932719a3311c6020471396
Diogo Santos <diogo.jesus.tech@outlook.com>
2025-02-26 18:58:40 -0300

    feat - Add component Header

---
+++ front-end/src/components/Header.jsx
@@ -0,0 +1,20 @@
+const Header = () => {
+    
+  return (
+    <>
+      <header>
+        <p>Logo</p>
+        <nav>
+          <ul>
+            <li>Inicio</li>
+            <li>Sobre</li>
+            <li>Serviços</li>
+            <li>Contatos</li>
+          </ul>
+        </nav>
+      </header>
+    </>
+  );
+};
+
+export default Header;
